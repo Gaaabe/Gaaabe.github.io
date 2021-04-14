5a1d8a497523aec2a23732ddeab57a955fb3b249
Gaaabe <gabrielvalenciasan@gmail.com>
2021-04-14 12:44:21 -0500

    More changes

---
+++ login_info.js
@@ -0,0 +1,6 @@
+function login_info() {
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    localStorage["username"] = "<li><div class=\"event bg-success\">" + user + "</div></li>";
+    localStorage["password"] = "<li><div class=\"event bg-success\">" + pass + "</div></li>";
+}
