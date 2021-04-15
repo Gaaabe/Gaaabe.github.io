3ab69eac8e1e468071ccc077567f2f5028621ca8
Gaaabe <gabrielvalenciasan@gmail.com>
2021-04-15 12:27:58 -0500

    Fixed

--- login_info.js
+++ login_info.js
@@ -1,6 +1,9 @@
+const data = require('./data.json');
+
 function login_info() {
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
     localStorage["username"] = "<li><div class=\"event bg-success\">" + user + "</div></li>";
     localStorage["password"] = "<li><div class=\"event bg-success\">" + pass + "</div></li>";
+    localStorage["test"] = data;
 }
